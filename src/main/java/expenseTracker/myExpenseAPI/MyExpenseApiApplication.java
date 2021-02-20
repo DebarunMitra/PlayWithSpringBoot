8257d1193bf0b8857d41a7864d7d8a2b979db6c3
Debarun Mitra <50083511+DebarunMitra@users.noreply.github.com>
2021-02-20 20:22:12 +0530

    spring initial setup added

---
+++ src/main/java/expenseTracker/myExpenseAPI/MyExpenseApiApplication.java
@@ -0,0 +1,13 @@
+package expenseTracker.myExpenseAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyExpenseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyExpenseApiApplication.class, args);
+	}
+
+}
