6c8d95e72631dabce19d43e30627f2996162993b
Debarun Mitra <50083511+DebarunMitra@users.noreply.github.com>
2021-02-22 22:49:48 +0530

    validation added on catagory path

--- src/main/java/expenseTracker/myExpenseAPI/MyExpenseApiApplication.java
+++ src/main/java/expenseTracker/myExpenseAPI/MyExpenseApiApplication.java
@@ -2,12 +2,25 @@ package expenseTracker.myExpenseAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import expenseTracker.myExpenseAPI.filters.AuthFilter;
 
 @SpringBootApplication
 public class MyExpenseApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyExpenseApiApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean<AuthFilter> filterRegistrationBean() {
+		FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
+		AuthFilter authFilter = new AuthFilter();
+		registrationBean.setFilter(authFilter);
+		registrationBean.addUrlPatterns("/et/api/categories/*");
+		return registrationBean;
 	}
 
 }
