ce6aa000bbc50a432524c0e86972debda29d0118
Debarun Mitra <50083511+DebarunMitra@users.noreply.github.com>
2021-03-04 16:38:15 +0530

    package structure updated

---
+++ StudentsEntryRESTapi/src/main/java/studentsEntry/studententry/StudententryApplication.java
@@ -0,0 +1,13 @@
+package studentsEntry.studententry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudententryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudententryApplication.class, args);
+	}
+
+}
---
+++ StudentsEntryRESTapi/src/test/java/studentsEntry/studententry/StudententryApplicationTests.java
@@ -0,0 +1,13 @@
+package studentsEntry.studententry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudententryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
