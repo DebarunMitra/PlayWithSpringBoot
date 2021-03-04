d25e8864e35df4ca66ede86cc735c235a23815e1
Debarun Mitra <50083511+DebarunMitra@users.noreply.github.com>
2021-03-04 23:11:53 +0530

    config created

---
+++ StudentsEntryRESTapi/src/main/java/studentsEntry/studententry/Configs/StudentConfig.java
@@ -0,0 +1,27 @@
+package studentsEntry.studententry.Configs;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import studentsEntry.studententry.Repository.StudentRepository;
+import studentsEntry.studententry.models.Students;
+
+@Configuration
+public class StudentConfig {
+    
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Students deba = new Students("deba", "deba@t.in", LocalDate.of(2000, Month.JANUARY, 5), 21);
+            Students mitra = new Students("mitra", "mitra@t.in", LocalDate.of(2000, Month.OCTOBER, 4), 20);
+            repository.saveAll(
+                List.of(deba, mitra)
+            );
+        };
+    }
+}
