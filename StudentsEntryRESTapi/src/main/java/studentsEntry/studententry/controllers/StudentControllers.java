85b346893971ec59ec789f083457caf55ac60b58
Debarun Mitra <50083511+DebarunMitra@users.noreply.github.com>
2021-03-05 18:34:16 +0530

    Put API added

--- StudentsEntryRESTapi/src/main/java/studentsEntry/studententry/controllers/StudentControllers.java
+++ StudentsEntryRESTapi/src/main/java/studentsEntry/studententry/controllers/StudentControllers.java
@@ -11,8 +11,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import studentsEntry.studententry.models.Students;
@@ -43,5 +45,14 @@ public class StudentControllers {
     @DeleteMapping("/{studentId}")
     public void deleteStudent(@PathVariable("studentId") Long studentId){
         studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping("/{studentId}")
+    public void updateStudent(
+        @PathVariable("studentId") Long studentId,
+        @RequestParam(required = false) String name,
+        @RequestParam(required = false) String email
+        ){
+        studentService.updateStudent(studentId, name, email);
     }
 }
--- StudentsEntryRESTapi/src/main/java/studentsEntry/studententry/services/StudentService.java
+++ StudentsEntryRESTapi/src/main/java/studentsEntry/studententry/services/StudentService.java
@@ -1,7 +1,10 @@
 package studentsEntry.studententry.services;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -44,4 +47,25 @@ public class StudentService {
 
         studentRepository.deleteById(studentId);
 	}
+
+
+    @Transactional
+	public void updateStudent(Long studentId, String name, String email) {
+	
+         Students student = studentRepository.findById(studentId)
+                 .orElseThrow(() -> new IllegalStateException("Student Not Found"));
+
+         if(name != null && name.length() > 0 && !Objects.equals(student.getName(), name)){
+             student.setName(name);
+         }
+
+         if(email !=null && email.length()>0 && !Objects.equals(student.getEmail(), email)){
+             Optional<Students> studentOptional = studentRepository.findStudentByEmail(email);
+
+             if(studentOptional.isPresent()){
+                 throw new IllegalStateException("Email already taken");
+             }
+             student.setEmail(email);
+         }
+    }
 }
